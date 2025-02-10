0ee533eed87012963f3729c10b19b2fdc88007ff
André Dias <diasa0421@gmail.com>
2025-02-10 11:12:06 -0300

    Update problemaalturamedia.java

--- problemaalturamedia.java
+++ problemaalturamedia.java
@@ -7,12 +7,22 @@ public class problemaalturamedia {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner (System.in);
 		
+		System.out.print("Diga o nome da Pessoa 1: ");
+		String nome = scanner.nextLine();
+		
 		System.out.print("Diga a altura da Pessoa 1: ");
 		double altura = scanner.nextDouble();
+		scanner.nextLine();
 		
-		System.out.print("Diga o nome da Pessoa 1");
-		String nome = scanner.nextLine();
-
+		System.out.print("Diga o nome da Pessoa 2: ");
+		String nome2 = scanner.nextLine();
+		
+		System.out.print("Diga a altura de" + nome2);
+		double altura2 = scanner.nextDouble();
+		
+		double alturamedia  = (altura + altura2 /2);
+		
+		System.out.printf("A altura média entre %s e %s é: %.1f metros%n", nome, nome2, alturamedia);
 	}
 
 }
